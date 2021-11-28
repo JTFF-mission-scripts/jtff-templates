459838e4c9f3a276497ef29c2eb469ef2845ba5e
Alexandre PROUST <alexproust@gmail.com>
2021-11-28 14:14:56 +0100

    Add Truman on syrian template
    Truman airboss event is 90min. Can be adjust later

--- settings/syria/airboss-syria.lua
+++ settings/syria/airboss-syria.lua
@@ -40,8 +40,54 @@ AirBossConfig = {
             marshall = 'MarshallRelay',
             lso = 'LSORelay',
         },
-        singlecarrier = true,
+        singlecarrier = false,
         event_duration_minutes = 60,
+        event_ia_reserved_minutes = 15,
+        operationsstatspath = "C:/airboss-stats",
+        operationstrapsheetpath = "C:/airboss-trapsheets"
+    },
+    {
+        enable = true,
+        carriername = 'CSG-2 CVN-75',
+        alias = 'truman',
+        coalition = 'blue',
+        enable_menumarkzones = true,
+        enable_menusmokezones = true,
+        enable_niceguy = true,
+        handleAI = true,
+        tacan = {
+            channel = 75,
+            mode = 'X',
+            morse = 'TRU',
+        },
+        icls = {
+            channel = 11,
+            morse = 'TRULSO',
+        },
+        freq = {
+            base = 127.3,
+            marshall = 127.5,
+            lso = 127.4
+        },
+        infintepatrol = true,
+        controlarea = 65,
+        recoverycase = 1,
+        maxpatterns = 5,
+        maxstacks = 8,
+        difficulty = AIRBOSS.Difficulty.NORMAL,
+        menurecovery = {
+            enable = true,
+            duration = 30,
+            windondeck = 30,
+            offset = 0,
+            uturn = true
+        },
+        releayunit = {
+            marshall = 'MarshallRelay-75',
+            lso = 'LSORelay-75',
+        },
+        singlecarrier = false,
+        event_duration_minutes = 90,
         event_ia_reserved_minutes = 15,
         operationsstatspath = "C:/airboss-stats",
         operationstrapsheetpath = "C:/airboss-trapsheets"
