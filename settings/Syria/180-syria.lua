00e6532f73f72740f082be335bc067d6b2018fa7
Sebastien LONGO <sebastien.longo@oufy.net>
2022-01-27 16:08:34 +0100

    add specific script for each theatre

---
+++ settings/Syria/180-syria.lua
@@ -0,0 +1,21 @@
+triggerOnDemandTanker(
+        "tx2",
+        nil,
+        nil,
+        nil,
+        GROUP:FindByName("sea-anchor-2"):GetCoordinate()
+)
+triggerOnDemandTanker(
+        "ar6",
+        nil,
+        nil,
+        nil,
+        GROUP:FindByName("sea-anchor-3"):GetCoordinate()
+)
+triggerOnDemandTanker(
+        "sh3",
+        nil,
+        nil,
+        nil,
+        GROUP:FindByName("ground-anchor-1"):GetCoordinate()
+)
