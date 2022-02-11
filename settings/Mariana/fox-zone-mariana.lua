0e5d9542a4a33df0789d749e1d490356343f4460
Sebastien LONGO <sebastien.longo@oufy.net>
2022-02-11 19:00:29 +0100

    feat(mariana): move Carrier to east + Add a CAP zone training at east + move tx2 to east

--- settings/Mariana/fox-zone-mariana.lua
+++ settings/Mariana/fox-zone-mariana.lua
@@ -10,5 +10,17 @@ FoxRangesConfig= {
         f10Menu = false,
         missileLaunchMessages = false,
         missileDestruction = true
+    },
+    {
+        name = 'East',
+        enable = true,
+        safeZoneName = 'red_east_engage_zone',
+        launchZoneName = 'red_east_engage_zone',
+        --safeZoneGroupName = 'engage zone Red',
+        --launchZoneGroupName = 'engage zone Red',
+        debug = false,
+        f10Menu = false,
+        missileLaunchMessages = false,
+        missileDestruction = true
     }
 }
--- settings/Mariana/training-cap-navy-mariana.lua
+++ settings/Mariana/training-cap-navy-mariana.lua
@@ -28,5 +28,35 @@ TrainingCAPConfig = {
             engageFloor = 5000,
             engageCeiling = 55000
         }
+    },
+    {
+        enable = true,
+        coalitionCAP = coalition.side.RED,
+        name = 'East',
+        --patrolZoneGroupName = 'CAP zone Red',
+        --engageZoneGroupName = 'engage zone Red',
+        patrolZoneName = 'red_east_patrol_zone',
+        engageZoneName = 'red_east_engage_zone',
+        CAPGoups = {
+            'REDNavyCAP-9',
+            'REDNavyCAP-10',
+            'REDNavyCAP-11',
+            'REDNavyCAP-12',
+            'REDNavyCAP-13',
+            'REDNavyCAP-14',
+            'REDNavyCAP-15',
+            'REDNavyCAP-16'
+        },
+        skill = 'Excellent',
+        capParameters = {
+            patrolFloor = 20000,
+            patrolCeiling = 40000,
+            minPatrolSpeed = 250,
+            maxPatrolSpeed = 400,
+            minEngageSpeed = 250,
+            maxEngageSpeed = 900,
+            engageFloor = 5000,
+            engageCeiling = 55000
+        }
     }
 }
