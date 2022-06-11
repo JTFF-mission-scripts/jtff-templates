ae2e57117870927fe38a64999d7d8c89ca5c2866
Sebastien LONGO <sebastien.longo@oufy.net>
2022-06-11 11:17:39 +0200

    Persian: make BandareJask SA11 Range activated

--- settings/PersianGulf/range-persian.lua
+++ settings/PersianGulf/range-persian.lua
@@ -1,33 +1,14 @@
 RangeConfig = {
     {
-        name = "Akamas",
+        name = "Bandar-e-Jask",
         enable = false,
         benefit_coalition = coalition.side.BLUE,
         subRange = {
             {
-                name = "North",
-                groupsToSpawn = {"Strafe truck  Akamas", "Humvee JTAC Akamas"},
-                holdFire = true,
-                AI = false
-            },
-            {
-                name = "Center",
-                groupsToSpawn = {"Humvee JTAC Akamas-1", "Compound Akamas", "Compound soldat", "Manpad Compound",
-                                 "Compound vehicule", "AAA Compound", "Compound vehicule civil", "BTR Compound",
-                                 "SAM SA6 Akamas"},
-                holdFire = true,
-                AI = false
-            },
-            {
-                name = "Training Target",
-                groupsToSpawn = {"Cercle Akamas", "Strafe Akamas"},
-                holdFire = true,
-                AI = false
-            },
-            {
                 name = "SAM",
-                groupsToSpawn = {"SAM SA2 Akamas"},
-                holdFire = false
+                groupsToSpawn = {"Ground-18"},
+                holdFire = true,
+                AI = false
             }
         }
     }
